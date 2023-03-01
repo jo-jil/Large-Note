ab1abc15ac3b11d2bcb054133f3352168ef26c35
Joey Tschopp <jtschopp@u.rochester.edu>
2023-02-28 20:12:59 -0500

    testing out defferent displays

--- App.js
+++ App.js
@@ -15,7 +15,8 @@ function HomeScreen({ navigation }) {
         title="Go to B"
         onPress={() => navigation.navigate('B')}
       />
-    </View>
+      </View>
+
   );
 }
 
@@ -36,6 +37,74 @@ function Bscreen() {
 }
 const Stack = createNativeStackNavigator();
 
+// Styles
+const styles = {
+  app: {
+    flex: 4, // the number of columns you want to devide the screen into
+    marginHorizontal: "auto",
+    width: 400,
+    backgroundColor: "red"
+  },
+  row: {
+    flexDirection: "row"
+  },
+  "1col":  {
+    backgroundColor:  "lightblue",
+    borderColor:  "#fff",
+    borderWidth:  1,
+    flex:  1
+  },
+  "2col":  {
+    backgroundColor:  "green",
+    borderColor:  "#fff",
+    borderWidth:  1,
+    flex:  2
+  },
+  "3col":  {
+    backgroundColor:  "orange",
+    borderColor:  "#fff",
+    borderWidth:  1,
+    flex:  3
+  },
+  "4col":  {
+    flex:  4
+  }
+};
+
+// RN Code
+const Col = ({ numRows, children }) => {
+  return  (
+    <View style={styles[`${numRows}col`]}>{children}</View>
+  )
+}
+
+const Row = ({ children }) => (
+  <View style={styles.row}>{children}</View>
+)
+
+function App()  {
+  return (
+    <View style={styles.app}>
+      <Row>
+        <Col numRows={2}>
+          <Text>First column</Text>
+        </Col>
+        <Col numRows={2}>
+          <Text>Second column</Text>
+        </Col>
+      </Row>
+      <Row>
+        <Col numRows={1}>
+          <Text>First column</Text>
+        </Col>
+        <Col numRows={3}>
+          <Text>Second Column</Text>
+        </Col>
+      </Row>
+    </View>
+  )
+}
+/*
 function App() {
   return (
     <NavigationContainer>
@@ -46,6 +115,6 @@ function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+}*/
 
 export default App;
